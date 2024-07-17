4fdd04a8e50430f62c04347d0852bd154376bb0a
João Pedro <115329370+jpbdalmagro@users.noreply.github.com>
2024-07-17 13:36:46 -0300

    Atualização de código
    Corrigindo falhas e adicionando novas funções.

--- ex072(Número por extenso).py
+++ ex072(Número por extenso).py
@@ -1,10 +1,20 @@
-numero = 0
 numeros = ('um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nome', 'dez', 'onze', 'doze', 'treze',
            'quatorze', 'quinze' 'desesseis', 'desessete', 'dezoito', 'dezenove', 'vinte')
 
-while not 0 < numero < 21:
-    numero = int(input("Digite um número de 1 a 20: "))
-    if not 0 < numero < 21:
+while True:
+    while True:
+        numero = int(input("Digite um número de 1 a 20: "))
+        if 0 < numero <= 20:
+            break
         print("Número inválido!")
 
-print(f"Você digitou o número {numeros[numero - 1]}.")
+    print(f"Você digitou o número {numeros[numero - 1]}.")
+    while True:
+        flag = str(input("Quer continuar? ")).strip().lower()[0]
+        if flag in 'sny':
+            break
+        print("Não entendi.")
+    if flag in 'n':
+        break
+
+print("Até mais!")
